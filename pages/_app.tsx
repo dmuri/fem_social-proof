3524b6d735aae594e990a11bcb5ea489ce725de0
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-10 11:49:30 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
+import Head from 'next/head'
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>Frontend Mentor Challenge</title>
+        </Head>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </>
+  )
 }
-
